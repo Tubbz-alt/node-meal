2e3068eb895b35edb6ffbbf9db0e8c8570dd1d78
Feng Wu <Feng.Wu@sandisk.com>
2017-09-26 16:30:55 +0800

    Mod

--- ntlmAuth.js
+++ ntlmAuth.js
@@ -1,12 +1,12 @@
 exports.options = {
-	username: Buffer.from([ 49, 54, 52, 50, 55 ]).toString(),
+	username: '12345',
 	lm_password: new Buffer([ 249, 228, 175, 198, 91, 141, 168, 223, 148, 203, 241, 101, 239, 29, 159, 90 ]),
 	nt_password: new Buffer([ 27, 172, 52, 93, 137, 162, 105, 90, 186, 74, 116, 114, 82, 11, 176, 165 ]),
 };
 
 if(require.main === module) {
 	const httpntlm = require('httpntlm');
-	const password = `abc`;
+	const password = 'abc';
 	console.dir(httpntlm.ntlm.create_LM_hashed_password(password));
 	console.dir(httpntlm.ntlm.create_NT_hashed_password(password));
 }
